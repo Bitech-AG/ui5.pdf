d0bdeb84e9bf86e4b9bb6531e41159ef858a8f25
Richard Martens <richard.martens@bitech.ag>
2019-10-23 07:19:15 +0200

    placing a consumer application. v0.4.1

---
+++ test/consumer/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ui5lab.rm.file.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
